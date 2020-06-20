5e8fd3895d175b42ece8c1786d8da3dd6319ed6b
dselarka <dselarka@iu.edu>
2020-06-19 23:51:41 -0400

    Working version (alpha)

--- ypd.py
+++ ypd.py
@@ -12,8 +12,16 @@ for links in soup.find_all('a'):
     if(link[0:6]=="/watch" and link[0]!="#"):
         link="https://www.youtube.com"+link
         link = str(link)
+        #print(link)
         playlist.append(link)
 
 playlist = list(dict.fromkeys(playlist))
 print(len(playlist))
 
+video_quality = input("Enter the video quality to download (1080p,720p,360p,240p,144p): ")
+for link in playlist:
+    yt = YouTube(link)
+    videos = yt.streams.filter(mime_type="video/mp4", res=video_quality)
+    video = videos[0]
+    video.download("Desktop")
+    print(yt.title + "has been downloaded.")
